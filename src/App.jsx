5158876acd15755952027cf542e624ccf1c86d12
Jordan Moreno <jordam4@uci.edu>
2026-03-27 02:12:43 -0700

    Add LPN webring footer navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import WebringNav from './components/WebringNav'
 import NavDock from './components/NavDock'
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <About />
       <Projects />
       <Contact />
+      <WebringNav />
       <NavDock />
     </>
   )
